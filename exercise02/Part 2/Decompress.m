38f223104390a20e5e9627bb507b13c764ee4004
Jan Rüegg <rggjan@gmail.com>
2011-03-09 09:46:14 +0100

    + Finished decompression function

--- exercise02/Part 2/Decompress.m
+++ exercise02/Part 2/Decompress.m
@@ -1,5 +1,3 @@
 function I_rec = Decompress(I_comp)
-
-% Your decompression code goes here!
-
-I_rec = I_comp.I; % this is just a stump to make the evaluation script run, replace it with your code!
+  I_rec = I_comp.Uk*I_comp.Z + I_comp.mu;
+end
