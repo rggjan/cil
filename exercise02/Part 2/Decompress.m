8f2d918b6aa65b849b5008270249d2876281fc07
Jan Rüegg <rggjan@gmail.com>
2011-03-09 10:04:49 +0100

    + Started Fixing decompression function

--- exercise02/Part 2/Decompress.m
+++ exercise02/Part 2/Decompress.m
@@ -1,3 +1,8 @@
 function I_rec = Decompress(I_comp)
-  I_rec = I_comp.Uk*I_comp.Z + I_comp.mu;
+  I_rec = I_comp.Uk*I_comp.Z + repmat(I_comp.mu, 1, size(I_comp.Z, 2));
+
+  dims = size(I_rec)
+  d = sqrt(dims(1))
+
+  I_rec = reshape(I_rec, d, []);
 end
