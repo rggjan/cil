022fec9ef68895963223e7a6a088e5628b6d8815
Jan Rüegg <rggjan@gmail.com>
2011-03-16 08:38:31 +0100

    + Added mean column prediction

--- exercise02/PredictMissingValues.m
+++ exercise02/PredictMissingValues.m
@@ -4,4 +4,11 @@ function X_pred = PredictMissingValues(X, nil)
 
 % your collaborative filtering code here!
 X_pred = X;
+
+for i = 1:size(X, 1)
+  row = X_pred(i, :);
+  row(row == nil) = mean(row(row ~= nil));
+  X_pred(i, :) = row;
+end
+
 X_pred(X_pred == nil) = mean(X_pred(X_pred ~= nil));
