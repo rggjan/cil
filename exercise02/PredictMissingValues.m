6a94e9479806f59d005c8cbdf7c8245526a1edb1
Jan Rüegg <rggjan@gmail.com>
2011-03-16 08:50:11 +0100

    + Better averages

--- exercise02/PredictMissingValues.m
+++ exercise02/PredictMissingValues.m
@@ -5,10 +5,20 @@ function X_pred = PredictMissingValues(X, nil)
 % your collaborative filtering code here!
 X_pred = X;
 
+column_pred = zeros(size(X));
+row_pred = zeros(size(X));
+
 for i = 1:size(X, 1)
   row = X_pred(i, :);
   row(row == nil) = mean(row(row ~= nil));
-  X_pred(i, :) = row;
+  row_pred(i, :) = row;
 end
 
-X_pred(X_pred == nil) = mean(X_pred(X_pred ~= nil));
+for i = 1:size(X, 2)
+  column = X_pred(:, i);
+  column(column == nil) = mean(column(column ~= nil));
+  column_pred(:, i) = column;
+end
+
+average = (column_pred + 2*row_pred)/3;
+X_pred(X_pred == nil) = average(X_pred == nil);
