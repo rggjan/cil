2e73275be7b9bb1d0f32bd3e0d0a2e0c865126e5
Jan Rüegg <rggjan@gmail.com>
2011-03-16 09:31:05 +0100

    + Made a function out of it

--- exercise02/PredictMissingValues.m
+++ exercise02/PredictMissingValues.m
@@ -1,38 +1,38 @@
 function X_pred = PredictMissingValues(X, nil)
-% Predict missing entries in matrix X based on known entries. Missing
-% values in X are denoted by the special constant value nil.
+  % Predict missing entries in matrix X based on known entries. Missing
+  % values in X are denoted by the special constant value nil.
 
-% your collaborative filtering code here!
+  % your collaborative filtering code here!
 
-column_pred = zeros(size(X));
-row_pred = zeros(size(X));
+  column_pred = zeros(size(X));
+  row_pred = zeros(size(X));
 
-for i = 1:size(X, 1)
-  row = X(i, :);
-  row(row == nil) = mean(row(row ~= nil));
-  row_pred(i, :) = row;
+  for i = 1:size(X, 1)
+    row = X(i, :);
+    row(row == nil) = mean(row(row ~= nil));
+    row_pred(i, :) = row;
+  end
+
+  for i = 1:size(X, 2)
+    column = X(:, i);
+    column(column == nil) = mean(column(column ~= nil));
+    column_pred(:, i) = column;
+  end
+
+  X_average = X;
+  average = (column_pred + 2*row_pred)/3;
+  X_average(X_average == nil) = average(X_average == nil);
+
+  [U, D, V] = svd(X_average);
+
+  U = U*sqrt(D);
+  V = sqrt(D)*V;
+
+  U = U(:, 1:10);
+  V = V(:, 1:10);
+
+  prediction = U*V';
+
+  X_pred = X;
+  X_pred(X_pred == nil) = prediction(X_pred == nil);
 end
-
-for i = 1:size(X, 2)
-  column = X(:, i);
-  column(column == nil) = mean(column(column ~= nil));
-  column_pred(:, i) = column;
-end
-
-X_average = X;
-average = (column_pred + 2*row_pred)/3;
-X_average(X_average == nil) = average(X_average == nil);
-
-size(X_average)
-[U, D, V] = svd(X_average);
-
-U = U*sqrt(D);
-V = sqrt(D)*V;
-
-U = U(:, 1:10);
-V = V(:, 1:10);
-
-prediction = U*V';
-
-X_pred = X;
-X_pred(X_pred == nil) = prediction(X_pred == nil);
