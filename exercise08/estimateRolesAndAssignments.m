f990b185f940649d9d79346f87219f1b0f518ebd
Jan Rüegg <rggjan@gmail.com>
2011-04-20 08:40:43 +0200

    + Added Z calculation step

--- exercise08/estimateRolesAndAssignments.m
+++ exercise08/estimateRolesAndAssignments.m
@@ -1,6 +1,25 @@
 function [ Z, U ] = estimateRolesAndAssignments(X)
-%this function is just a stub,
-%you should write your own code here to solve the role mining problem
+  K = 2;
+
+  [perm_size, user_size] = size(X);
+
+  perm = randperm(user_size);
+  U = X(:, perm(1:K));
+
+  % Calculation of Z
+  Z = zeros(K, user_size);
+  for user = 1:user_size
+    big_matrix = abs(U - repmat(X(:,user), 1, K));
+    sums = sum(big_matrix, 1);
+    [dev_null, index] = min(sums);
+    Z(index, user) = 1;
+  end
+
+  % Calculation of U
+
+
+
+  %%%%%%%%%%%%%%%%%%%%%%%%%%
   U = X(:,1:2);
   Z = zeros(2,size(X,2));
   Z(1,1) = 1;
