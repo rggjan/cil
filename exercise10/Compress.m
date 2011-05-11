440b98bed4df6cfac1d4d1ca47c0a2b5c1b3419c
Jan Rüegg <rggjan@gmail.com>
2011-05-11 09:22:19 +0200

    + Added color support

--- exercise10/Compress.m
+++ exercise10/Compress.m
@@ -1,20 +1,22 @@
 function I_comp = Compress(I)
 
 % Padding preparation
-[h, w] = size(I);
+[h, w, c] = size(I);
 N = 2^(ceil(log2(max(h,w))));
 
 % Create image
-image = zeros(N, N);
-image(1:h, 1:w) = I;
+image = zeros(N, N, c);
+image(1:h, 1:w, :) = I;
 
 % Haar transform
 H = haarTrans(N);
-transformed_image = H'*image*H;
+for i=1:c
+  transformed_image(:,:,c) = H'*image(:,:,c)*H;
+end
 
 % Thresholding
 transformed_image(abs(transformed_image)<0.5) = 0;
 
 I_comp.h = h;
 I_comp.w = w;
-I_comp.data = sparse(transformed_image);
+I_comp.data = sparse(reshape(transformed_image, N, c*N));
--- exercise10/Decompress.m
+++ exercise10/Decompress.m
@@ -1,8 +1,14 @@
 function I_rec = Decompress(I_comp)
 
-N = size(I_comp.data, 1);
+[N, N3] = size(I_comp.data);
+C = N3/N;
+
+data = reshape(full(I_comp.data), N, N, C);
 
 H = haarTrans(N);
-I_rec = H*I_comp.data*H';
 
-I_rec = I_rec(1:I_comp.h, 1:I_comp.w);
+for c=1:C
+  I_rec(:,:,c) = H*data(:,:,c)*H';
+end
+
+I_rec = I_rec(1:I_comp.h, 1:I_comp.w, :);
