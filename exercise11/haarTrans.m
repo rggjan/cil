be353fbbc6578f7830bfb23cef71018bd00f701a
Patrick Baenziger <patrbaen@student.ethz.ch>
2011-05-12 17:17:46 +0200

    Added new exercise 11

---
+++ exercise11/haarTrans.m
@@ -0,0 +1,23 @@
+function h=haarTrans(N)
+
+%   H = haarTrans returns the N-by-N HAAR transform matrix.
+%   H contains the Haar wavelet basis as columns vectors.
+%   If matrix A is square, the two-dimensional Haar transformation
+%   of A can be computed as H'*A*H.
+
+h=zeros(N,N); 
+h(1,1:N)=ones(1,N)/sqrt(N);
+
+for k=1:N-1 
+  p=fix(log(k)/log(2)); 
+  q=k-(2^p); 
+  k1=2^p;
+  t1=N/k1; 
+  k2=2^(p+1);
+  t2=N/k2; 
+  for i=1:t2 
+    h(k+1,i+q*t1) = (2^(p/2))/sqrt(N); 
+    h(k+1,i+q*t1+t2) = -(2^(p/2))/sqrt(N); 
+  end 
+end
+h = h';
