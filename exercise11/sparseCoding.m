454d5113d0db4e58d30b85c25da3ea87ef94d999
Jan Rüegg <rggjan@gmail.com>
2011-05-18 09:07:58 +0200

    + Finished sparsecoding

--- exercise11/sparseCoding.m
+++ exercise11/sparseCoding.m
@@ -18,6 +18,9 @@ l = size(U,2);
 n = size(X,2);
 
 Z = zeros(l,n);
+
+num_atoms = size(U, 2);
+
 % Loop over all observations in the columns of X
 for nn = 1:n
     
@@ -25,22 +28,37 @@ for nn = 1:n
     % For the modification with masking make sure that you only take into
     % account the known observations defined by the mask M
     % Initialize z to zero
-   
-    % TO BE FILLED
+
+    R = X(:, nn);
+    masked_R = M*R;
+    M = diag(M(:, nn));
+    % TODO use vector instead of Matrix M
+    z = Z(:, nn);
     
-    while (TO BE FILLED)
-        
-        % TO BE FILLED 
-        
+    threshold = sigma*norm(X(:, nn));
+    rc_max = +inf;
+
+    while (norm(masked_R) > threshold && (rc_max > rc_min))
         % Select atom with maximum absolute correlation to the residual       
-        
         % Update the maximum absolute correlation
+        rc_max = 0;
+        best_atom = 0;
+
+        for i = 1:num_atoms
+          rc = R'*U(:, i);
+          if rc > rc_max
+            rc_max = rc;
+            best_atom = i;
+          end
+        end
         
         % Update coefficient vector z and residual z
+        R = R - rc_max*U(:, best_atom);
+        z(best_atom) += rc_max;
         
         % For the inpainting modification make sure that you only consider
         % the known observations defined by the mask M
-        
+        masked_R = M*R;
     end
     
     % Add the calculated coefficient vector z to the overall matrix Z
