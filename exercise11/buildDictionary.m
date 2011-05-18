0c93db0aa173c049e35a74ee215076452eb3b095
Patrick Baenziger <patrbaen@student.ethz.ch>
2011-05-18 08:41:21 +0200

    Dictionary building, most trivial implementation

--- exercise11/buildDictionary.m
+++ exercise11/buildDictionary.m
@@ -9,3 +9,9 @@ function U = buildDictionary(dim)
 % U (d x l) dictionary with unit norm atoms
 
 % Here you add the code for getting the dictionary you desire
+
+
+% Most trivial implementation, using both DCT and haarTransform
+% Is still a TODO
+
+U = [overDCTdict(dim, 50) haarTrans(dim)];
