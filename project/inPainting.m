7f1f01eae992893f1479b0ab9340a49ef2e14019
Jan Rüegg <rggjan@gmail.com>
2011-05-26 17:25:22 +0200

    + Nice splitted image

--- project/inPainting.m
+++ project/inPainting.m
@@ -13,13 +13,43 @@ I = I .* mask;
 
 radius = 1;
 [height, width] = size(I);
+total = height * width;
 
 border_I = zeros(height+2*radius, width+2*radius);
 border_I((1+radius):(radius+height), (1+radius):(radius+width)) = I;
 
+border_mask = zeros(height+2*radius, width+2*radius);
+border_mask((1+radius):(radius+height), (1+radius):(radius+width)) = mask;
+
+X = zeros(8, total);
+maskX = zeros(8, total);
+
+counter = 1
 for x = (1+radius):(radius+width)
   for y = (1+radius):(radius+height)
-    border_I(x,y) = 1-border_I(x,y);
+    X(1, counter) = border_I(y+1, x+1);
+    X(2, counter) = border_I(y  , x+1);
+    X(3, counter) = border_I(y-1, x+1);
+    X(4, counter) = border_I(y+1, x  );
+    X(5, counter) = border_I(y-1, x  );
+    X(6, counter) = border_I(y+1, x-1);
+    X(7, counter) = border_I(y  , x-1);
+    X(8, counter) = border_I(y-1, x-1);
+
+    maskX(1, counter) = border_mask(y+1, x+1);
+    maskX(2, counter) = border_mask(y  , x+1);
+    maskX(3, counter) = border_mask(y-1, x+1);
+    maskX(4, counter) = border_mask(y+1, x  );
+    maskX(5, counter) = border_mask(y-1, x  );
+    maskX(6, counter) = border_mask(y+1, x-1);
+    maskX(7, counter) = border_mask(y  , x-1);
+    maskX(8, counter) = border_mask(y-1, x-1);
+
+    xsum=sum(X(:, counter));
+    masksum=sum(maskX(:, counter));
+    average = sum(X(:, counter))/sum(maskX(:, counter));
+    border_I(x,y) = average;
+    counter = counter + 1;
   end
 end
 
