c031bb5f76e96c037133b97b40d18362ed878d6c
Jan Rüegg <rggjan@gmail.com>
2011-06-08 15:32:14 +0200

    + Added dimensionreduction

---
+++ project/dimensionReduction.m
@@ -0,0 +1,31 @@
+function I_reduced = dimensionReduction(I, T, parameters)
+
+image_size = size(I, 1);
+  
+num_patches = image_size / parameters.patch_size;
+
+for i = 1 : num_patches
+  for j = 1 : num_patches
+    P_framed = I(parameters.patch_size * (i - 1) + 1 : ...
+                 parameters.patch_size * i + 2 * parameters.patch_frame, ...
+		 parameters.patch_size * (j - 1) + 1 : ...
+                 parameters.patch_size * j + 2 * parameters.patch_frame);
+
+    P_fft = fft2(P_framed);
+    P_abs = abs(P_fft);
+		  
+    P_fft(abs(P_fft) < T(i, j)) = 0;
+    P_ifft = abs(ifft2(P_fft));
+			
+    P_rec = P_ifft(parameters.patch_frame + 1 : ...
+                   end - parameters.patch_frame, ...
+                   parameters.patch_frame + 1 : ...
+                   end - parameters.patch_frame);
+			
+    range_i = parameters.patch_size * (i-1) + 1 : parameters.patch_size * i;
+    range_j = parameters.patch_size * (j-1) + 1 : parameters.patch_size * j;    
+    I_reduced(range_i, range_j) = P_rec;
+  end
+end
+
+end
