e5c12f2dbe8c573239a2274c52c5485b7a153195
Patrick Baenziger <patrbaen@student.ethz.ch>
2011-06-08 15:13:10 +0200

    Renamed parameter

--- project/inPainting.m
+++ project/inPainting.m
@@ -4,7 +4,7 @@ parameters.gauss_size = 5;
 parameters.gauss_sigma = 0.8;
 
 parameters.patch_size = 16;
-parameters.patch_frame = 8;
+parameters.patch_frame_size = 8;
 
 parameters.validation = 0.5; % Fraction of pixels used for validating the threshold
 parameters.iterations = 4;
