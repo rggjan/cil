7b9a4ffc2a5ab179add16e6ff62331c8e4a209bc
Patrick Baenziger <patrbaen@student.ethz.ch>
2011-06-15 17:51:31 +0200

    Beginning of global Optimization

--- project/OptimizeInpainting.m
+++ project/OptimizeInpainting.m
@@ -1,30 +1,86 @@
+function optimizeInpainting()
 % Optimize the parameters of inPainting.
 
-parameters.gauss_size = 5;
-parameters.gauss_sigma = 0.8;
+  %Initial parameters
+  parameters.gauss_size = 10;
+  parameters.gauss_sigma = 0.8;
+  parameters.patch_size = 16;
+  parameters.patch_frame_size = 8;
+  parameters.td_abortbelow_stdev = 0.0001;
+  parameters.td_abortbelow_stepsize = 0.01;
+  parameters.validation = 0.2; 
+  parameters.iterative = true; 
+  parameters.max_iterations = 1; 
+  parameters.abortbelow_change = 0.15; 
+  
+  % Result set
+  final_parameters = parameters;
+  cost = EvaluateInpaintingParameterized(parameters);
+  
+  while(true)
 
-parameters.patch_size = 16;
-parameters.patch_frame = 8;
+    %Working copy
+    new_parameters = parameters;
+    
+    
+    % Case +1
+    new_parameters.abortbelow_change = getNextAbortBelowChange(parameters.abortbelow_change,1)
+    new_cost_plus = EvaluateInpaintingParameterized(new_parameters) - cost
+    % Case -1
+    new_parameters.abortbelow_change = getNextAbortBelowChange(parameters.abortbelow_change,-1)
+    new_cost_minus = EvaluateInpaintingParameterized(new_parameters) - cost
 
-parameters.iterations = 1;
+    if(new_cost_plus > 0 && new_cost_minus > 0)
+        %Keep the old setting
+    else
+        final_parameters.abortbelow_change = getNextAbortBelowChange(parameters.abortbelow_change,-1*(new_cost_plus-new_cost_minus)/2);
+    end
+    
+    parameters = final_parameters
+    cost = EvaluateInpaintingParameterized(parameters)
+    
+  end
 
-error_old = 1;
-error = EvaluateInpaintingParameterized(parameters);
+end
 
-while abs(error_old - error) / error_old > 0.05
-	
-	parameters.gauss_size = 5;
-	parameters.gauss_sigma = 0.8;
-	
-	parameters.patch_size = 16;
-	parameters.patch_frame = 8;
-	
-	parameters.iterations = 1;
-	
-	error_old = error;
-	error = EvaluateInpaintingParameterized(parameters);
-	
-	disp(['Average quadratic error: ' error]);
-	disp(['Used parameters: ' parameters]);
-	
+function new = getNextGaussSize(Value, Stepsize)
+  
+end
+
+function new = getNextPatchSize(Value, Stepsize)
+
+  
+end
+
+function new = getNextFrameSize(Value, Stepsize)
+
+  
+end
+
+function new = getNextTDAbortBelowStep(Value, Stepsize)
+
+  
+end
+
+function new = getNextTDAbortBelowStdev(Value, Stepsize)
+
+  
+end
+
+function new = getNextValidation(Value, Stepsize)
+
+end
+
+function new = getNextMaxIterations(Value, Stepsize)
+
+end
+
+function new = getNextAbortBelowChange(Value, Stepsize)
+  new = Value + Stepsize/100;
+  if(new >= 1)
+    new = 1-eps;
+  end
+  if(new <= 0)
+      new = eps;
+  end
 end
