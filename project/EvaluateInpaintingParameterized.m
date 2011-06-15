08f75882dfbf492bded404bd02b4b3b0269d2d41
Patrick Baenziger <patrbaen@student.ethz.ch>
2011-06-15 17:51:09 +0200

    Fix wrong call

--- project/EvaluateInpaintingParameterized.m
+++ project/EvaluateInpaintingParameterized.m
@@ -1,42 +1,53 @@
 % Measure approximation error for several images.
 
-function error = EvaluateInpaintingParameterized(parameters)
+function cost = EvaluateInpaintingParameterized(parameters)
 
-file_list = dir(); 
-k = 1;
+  file_list = dir(); 
+  k = 1;
 
-Errors = []; % mean squared errors for each image
+  Errors = []; % mean squared errors for each image
+  Times = [];
 
-for i = 3 : length(dir) % running through the folder
-	
-	file_name = file_list(i).name; % get current filename
-	
-	% Only keep the images in the loop
-	if (length(file_name) < 5)
-		continue;
-	elseif ( max(file_name(end-4:end) ~= '2.png'))
-		continue;
-	end
-	mask_name = [file_name(1:end-4) '_mask.png'];
-		
-	% Read image, convert to double precision and map to [0,1] interval
-	I = imread(file_name);
-	I = double(I) / 255;
-	
-	% Read the respective binary mask
-	% EVALUATION IS DONE WITH A FIXED MASK
-	mask = imread(mask_name);
-	
-	I_mask = I;
-	I_mask(~mask) = 0;
-		  
-	% Call the main inPainting function
-	I_rec = inPainting(I_mask, mask);
-	
-	% Measure approximation error
-	Errors(k) = mean(mean(mean( ((I - I_rec) ).^2)));
-	
-	k = k+1;
+  for i = 3 : length(dir) % running through the folder
+
+    file_name = file_list(i).name; % get current filename
+
+    % Only keep the images in the loop
+    if (length(file_name) < 5)
+      continue;
+    elseif ( max(file_name(end-4:end) ~= '2.png'))
+      continue;
+    end
+    mask_name = [file_name(1:end-4) '_mask.png'];
+
+    % Read image, convert to double precision and map to [0,1] interval
+    I = imread(file_name);
+    I = double(I) / 255;
+
+    % Read the respective binary mask
+    % EVALUATION IS DONE WITH A FIXED MASK
+    mask = imread(mask_name);
+
+    I_mask = I;
+    I_mask(~mask) = 0;
+
+    % Call the main inPainting function
+    tic;
+    I_rec = inPaintingParameterized(I_mask, mask, parameters);
+    Times(k) = toc;
+
+    % Measure approximation error
+    Errors(k) = mean(mean(mean( ((I - I_rec) ).^2)));
+
+    k = k+1;
+  end
+
+  cost = costfunc(Errors, Times);
+
 end
 
-error = mean(Errors);
+function s = costfunc(error, time) 
+
+  s = exp(mean(error))+exp(mean(time*1e-5));
+
+end
