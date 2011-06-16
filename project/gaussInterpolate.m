992b68917da5359d471811b684156eb397e40353
Patrick Baenziger <patrbaen@student.ethz.ch>
2011-06-16 10:13:46 +0200

    Fix Nan issue

--- project/gaussInterpolate.m
+++ project/gaussInterpolate.m
@@ -5,6 +5,10 @@ mask = double(mask);
 h = fspecial('gaussian', parameters.gauss_size, parameters.gauss_sigma);
 I_masked = I .* mask;
 
-I_filtered = imfilter(I_masked, h, 'replicate') ./ ...
-             imfilter(mask, h, 'replicate');
+I_filtered = imfilter(I_masked, h, 'symmetric') ./ ...
+             imfilter(mask, h, 'symmetric');
 I_out = (I_filtered .* (1-mask)) + (I_masked .* (mask));
+
+%Make sure we eliminate NaNs that may occur,
+%when gauss is too small and we have a large hole
+I_out(isnan(I_out)) = mean(mean(I(logical(mask))));
