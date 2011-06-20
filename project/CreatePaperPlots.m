399238bf3ccdf7fda700b2a6537c8b63165ada7d
Patrick Baenziger <patrbaen@student.ethz.ch>
2011-06-20 17:43:52 +0200

    Fix output...

--- project/CreatePaperPlots.m
+++ project/CreatePaperPlots.m
@@ -1,3 +1,5 @@
+% Creates plots and figures used in the paper. (CAVEAT: Takes long!)
+
 % Create Error vs. MissingPixels Plot
 % Requires parameters saved in a parameters.mat file
 
@@ -5,6 +7,7 @@
 
   stepsize = 2;
   no_steps = floor(100/stepsize);
+  % Init arrays
   error_algo = zeros(no_steps, 1);
   error_base1 = zeros(no_steps, 1);
   error_base2 = zeros(no_steps, 1);
@@ -13,9 +16,8 @@
   
   fprintf('1) Calculating error vs. missing pixels\n')
   fprintf('1A) New algorithm\n')
-  %Use parallel computation for this
+  %Use parallel computation for this, if available
   parfor k=0:no_steps
-
     % Our algorithm
     [unused, e] = EvaluateInpaintingParameterized(parameters,stepsize*k/100);
     error_algo(k+1) = e/(k*stepsize/100);% Normalized
@@ -30,7 +32,7 @@
   end
   cd(main_path)
   
-  fprintf('1C) Baseline 1\n')
+  fprintf('1C) Baseline 2\n')
   cd('baseline2/')
   parfor k=0:no_steps
     %Baseline 1
