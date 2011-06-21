94b243af028d774b3a8f931ee82459d0543527c0
Patrick Baenziger <patrbaen@student.ethz.ch>
2011-06-21 16:09:51 +0200

    Added more output

--- project/baseline1/EvaluateInpaintingParameterized.m
+++ project/baseline1/EvaluateInpaintingParameterized.m
@@ -3,55 +3,44 @@
 function [avgQErr] = EvaluateInpaintingParameterized(missing_pixels_fract)  
 
   file_list = dir('..'); 
-  rep=3;
-  
   dir_length = length(dir('..'));
 
-  Errors_final = [];
-  LTimes_final = [];
+  Errors = []; % mean squared errors for each image
+  
+  for i = 3 : dir_length  % running through the folder
+    file_name = file_list(i).name; % get current filename
 
-  parfor r = 1 : rep
-     Errors = []; % mean squared errors for each image
-     LTimes = [];
-     for i = 3 : dir_length  % running through the folder
+    % Only keep the images in the loop
+    if (length(file_name) < 5)
+      continue;
+    elseif ( max(file_name(end-4:end) ~= '2.png'))
+      continue;
+    end
 
-        file_name = file_list(i).name; % get current filename
+    % Read image, convert to double precision and map to [0,1] interval
+    I = imread(strcat('../',file_name));
+    I = double(I) / 255;
 
-        % Only keep the images in the loop
-        if (length(file_name) < 5)
-          continue;
-        elseif ( max(file_name(end-4:end) ~= '2.png'))
-          continue;
-        end
-
-        % Read image, convert to double precision and map to [0,1] interval
-        I = imread(strcat('../',file_name));
-        I = double(I) / 255;
-
-        % Generate mask at random, with 60% missing pixels
-        mask = ones(size(I));
-        numpixels = size(I,1)*size(I,2);
-        holes = randperm(numpixels);
-        holes = holes(1:floor(missing_pixels_fract*numpixels));
-        mask(holes) = 0;
+    % Generate mask at random, with 60% missing pixels
+    mask = ones(size(I));
+    numpixels = size(I,1)*size(I,2);
+    holes = randperm(numpixels);
+    holes = holes(1:floor(missing_pixels_fract*numpixels));
+    mask(holes) = 0;
 
 
-        I_mask = I;
-        I_mask(~mask) = 0;
+    I_mask = I;
+    I_mask(~mask) = 0;
 
-        % Call the main inPainting function
-        tic;
-        I_rec = inPainting(I_mask, mask);
-        LTimes = [LTimes toc];
+    % Call the main inPainting function
+    I_rec = inPainting(I_mask, mask);
 
-        % Measure approximation error
-        Errors = [Errors mean(mean(mean( ((I - I_rec) ).^2)))];
-     end
-     Errors_final = [Errors_final Errors];
-     LTimes_final = [LTimes_final LTimes];
+    % Measure approximation error
+    Errors = [Errors mean(mean(mean( ((I - I_rec) ).^2)))];
   end
   
-  avgQErr = mean(Errors_final);
+  
+  avgQErr = mean(Errors);
 
 
 end
