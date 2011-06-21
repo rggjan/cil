4920b2e79c3c469624b9ba0f2671a4cbdfa27c7e
Patrick Baenziger <patrbaen@student.ethz.ch>
2011-06-21 15:43:10 +0200

    Add filenames to returned stdevs

--- project/EvaluateInpaintingParameterized.m
+++ project/EvaluateInpaintingParameterized.m
@@ -60,6 +60,7 @@ function [cost, avgQErr, stdev] = EvaluateInpaintingParameterized(parameters, mi
   % Determine mean and stdev per image
   % Count images first
   imc = 0;
+  filenames = {};
   for i = 3 : dir_length
     file_name = file_list(i).name; % get current filename
     % Only keep the images in the loop
@@ -68,17 +69,24 @@ function [cost, avgQErr, stdev] = EvaluateInpaintingParameterized(parameters, mi
       elseif ( max(file_name(end-4:end) ~= '2.png'))
        continue;
       end
+      %Count and save filenames for later
       imc = imc+1;
+      filenames{imc} = {file_name};
   end
   
-  stdev = zeros(imc,1);
+  for i=1:imc
+    stdev{i,2} = filenames{i};
+  end
+  
   if rep > 1
     for i=1:imc
-      stdev(imc,1) = std(Errors_final(i:imc:end));
+      stdev{i,1} = std(Errors_final(i:imc:end));
     end
   else
     % 1 Iteration - no deviation
-    stdev = zeros(imc,1);
+    for i=1:imc
+      stdev{i,1} = 0;
+    end
   end
   
     
